e4bde0a65b82a964fea9c88a7bb1b45f4fa34e14
KoKo <kngzhi@hotmail.com>
2022-03-31 22:33:39 +0800

    feat: bulk import from pages

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path')
 const data = require('./data.json')
 const urls = require('./urls.json')
 const async = require('async')
+const urlHandle = require('url')
 
 const TOML = (folder_name) => `[package]
 name = \"${folder_name}\"
@@ -21,7 +22,14 @@ const MAIN_CONTENT = `fn main() {
     println!("Hello, world!");
 }`
 
-async.mapLimit(urls.slice(5, 20).map(x => x[1]), 2, async function(url) {
+const LEARN_SECTION = 12
+
+const HOMEWORK_URLS = urls.filter(url => {
+    const no = Number(url[0].split(' ')[0].replace(/\.$/, ''))
+    return no < LEARN_SECTION + 1
+}).map(x => x[1])
+
+async.mapLimit(HOMEWORK_URLS.slice(30, 38), 2, async function(url) {
     const res = await fetch(url)
     const { body } = res
     await handleHtml(body, url)
@@ -38,8 +46,9 @@ async function handleHtml(html, url) {
         all_code_block[i] = $(this).text()
     })
 
+    const { origin, pathname } = urlHandle.parse(url)
     const urlInfos = url.split('/')
-    const FOLDER_NAME = urlInfos[urlInfos.length - 1].split('.')[0] + '_'
+    const FOLDER_NAME =  `.${pathname.replace('.html', '')}_`
     const FOLDER_PATH = path.resolve(`./practices/${FOLDER_NAME}`, '')
     const SRC_PATH = FOLDER_PATH + '/src'
     const BIN_PATH = SRC_PATH + '/bin'
@@ -49,8 +58,13 @@ async function handleHtml(html, url) {
 
     Promise.all(data.map(async (content, idx) => {
         if (!content) return
-        createFile(path.resolve(`${BIN_PATH}/${FOLDER_NAME}${idx}.rs`), content)
+        createFile(path.resolve(`${BIN_PATH}/${last(FOLDER_NAME.split('/'))}${idx}.rs`), content)
         createFile(path.resolve(`${FOLDER_PATH}/Cargo.toml`), TOML(FOLDER_NAME))
         createFile(path.resolve(`${SRC_PATH}/main.rs`), MAIN_CONTENT)
     }))
+}
+
+function last(arr) {
+    const len = arr.length
+    return arr[len - 1]
 }
